db3d0ac7183d96a3382becb7d5fc86a2a50a6451
Alansiapk <alansiapk0315@gmail.com>
2023-05-05 11:27:17 +0000

    added order in models and dal

---
+++ dal/orders.js
@@ -0,0 +1,82 @@
+const {Order, OrderStatus, OrderItem, User} = require('../models')
+
+const createOrder = async (orderData) => {
+    const order = new Order (orderData);
+    await order.save();
+
+    return order;
+}
+
+const createOrderItem = async (orderItemData) => {
+    const orderItem = new OrderItem (orderItemData)
+    await orderItem.save();
+
+    return orderItem;
+}
+
+const getAllOrders = async () => {
+    const orders = await Order.collection().fetch({
+        require: false,
+        withRelated: ['user', 'orderStatus', 'orderItems']
+    })
+    return orders;
+}
+
+const getAllStatuses = async () => {
+    const order_statuses = await OrderStatus.fetchAll().map((order_status) => {
+        return [order_status.get('id'), order_status.get('order_status')];
+    });
+    order_statuses.unshift([0, '---Select One---']);
+    return order_statuses;
+}
+
+const getOrderById = async (orderId) => {
+    return await Order.where({
+        id: orderId
+    }).fetch({
+        require:false,
+        withRelated:['user', 'orderStatus']
+    })
+}
+
+const getOrderItemsByOrderId = async (orderId) => {
+    return await OrderItem.where({
+        order_id: orderId
+    }).fetchAll({
+        require: false,
+        withRelated: ['product', 'product.category', 'product.brand']
+    })
+}
+
+const getOrdersByUserId = async (userId) => {
+    return await Order.where({
+        user_id: userId
+    }).fetchAll({
+        require: false,
+        withRelated: ['OrderStatus']
+    })
+}
+
+const updateStatus = async (orderId, newStatusId) => {
+    const order = await getOrderById(orderId);
+    order.set('order_status_id', newStatusId);
+    await order.save();
+    return order;
+}
+
+const deleteOrder = async (orderId) => {
+    const order = await getOrderById(orderId);
+    await order.destroy();
+}
+
+module.exports = {
+    createOrder,
+    createOrderItem,
+    getAllOrders,
+    getAllStatuses,
+    getOrderById,
+    getOrderItemsByOrderId,
+    getOrdersByUserId,
+    updateStatus,
+    deleteOrder,
+}
--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const bookshelf = require('../bookshelf')
 
 
 const Product = bookshelf.model('Product', {
-    tableName:'products',
+    tableName: 'products',
     category() {
         return this.belongsTo('Category')
     },
@@ -20,59 +20,92 @@ const Product = bookshelf.model('Product', {
     }
 });
 
-const Tag = bookshelf.model('Tag',{
+const Tag = bookshelf.model('Tag', {
     tableName: 'tags',
     products() {
         return this.belongsToMany('Product')
     }
 })
 
-const Category = bookshelf.model('Category',{
+const Category = bookshelf.model('Category', {
     tableName: 'categories',
     products() {
         return this.hasMany('Product', 'category_id');
     }
 })
 
-const Brand = bookshelf.model('Brand',{
+const Brand = bookshelf.model('Brand', {
     tableName: 'brands',
     products() {
         return this.hasMany('Product', 'brand_id');
     }
 })
 
-const Application = bookshelf.model('Application',{
+const Application = bookshelf.model('Application', {
     tableName: 'applications',
     products() {
         return this.hasMany('Product', 'application_id');
     }
 })
 
-const User = bookshelf.model('User',{
+const User = bookshelf.model('User', {
     tableName: 'users',
-    role(){
+    role() {
         return this.belongsTo("Role")
+    },
+    orders() {
+        return this.hasMany('Order')
     }
+
 })
 
-const Role = bookshelf.model('Role',{
+const Role = bookshelf.model('Role', {
     tableName: 'roles',
     users() {
         return this.hasMany('User', 'role_id');
     }
 })
 
+const BlacklistedToken = bookshelf.model('BlacklistedToken', {
+    tableName: 'blacklisted_tokens'
+})
 const CartItem = bookshelf.model('CartItem', {
     tableName: 'cart_items',
     product() {
         return this.belongsTo('Product')
     }
-
 })
 
-const BlacklistedToken = bookshelf.model('BlacklistedToken',{
-    tableName: 'blacklisted_tokens'
+const OrderItem = bookshelf.model('OrderItem', {
+    tableName: 'orders_items',
+    product() {
+        return this.belongsTo('Product')
+    },
+    order() {
+        return this.belongsTo('Order')
+    },
+    
 })
 
 
-module.exports = { Product, Category, Brand, Application, Tag, User, CartItem, Role, BlacklistedToken};
+const Order = bookshelf.model("Order", {
+    tableName: "orders",
+    user() {
+        return this.belongsTo("User");
+    },
+    orderStatus() {
+        return this.belongsTo("OrderStatus");
+    },
+    orderItems() {
+        return this.hasMany('OrderItem');
+    },
+});
+
+const OrderStatus = bookshelf.model("OrderStatus", {
+    tableName: "order_statuses",
+    orders() {
+        return this.hasMany('Order', 'order_id');
+    }
+})
+
+module.exports = { Product, Category, Brand, Application, Tag, User, CartItem, Role, BlacklistedToken, Order, OrderStatus, OrderItem };
