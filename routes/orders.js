1eb922def2454b1f2acdb520ebec58279c3aabcb
Alansiapk <alansiapk0315@gmail.com>
2023-05-16 03:44:44 +0000

    debug order item

--- routes/orders.js
+++ routes/orders.js
@@ -25,5 +25,23 @@ router.get("/", async (req, res) => {
     })
 })
 
+router.get('/:order_id/items', async (req, res) => {
+    const order = await getOrderById(req.params.order_id);
+    const orderItems = await getOrderItemsByOrderId(req.params.order_id);
+
+    const orderStatusForm = createOrderStatusForm(await getAllStatuses() );
+    orderStatusForm.fields.status_id.value = order.get('status_id');
+    res.render('orders/order-items', {
+        order: order.toJSON(),
+        orderItems: orderItems.toJSON(),
+        form: orderStatusForm.toHTML(bootstrapField)
+    })
+})
+
+router.post('/:order_id/update', async (req, res) => {
+    await updateStatus(req.params.order_id, req.body.order_status_id);
+    req.flash('success messages', 'Order status updated');
+    res.redirect(`/orders/${req.params.order_id}/items`)
+})
 
 module.exports = router;
